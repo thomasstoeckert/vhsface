472a0021cc6a66da965e8c35843f01f41f11c3fa
Thomas Stoeckert <Thomas@Stoeckert.us>
2015-10-01 11:45:15 -0400

    Added regular schedule times

--- src/scheduleInfo.c
+++ src/scheduleInfo.c
@@ -1,9 +1,9 @@
 #include <pebble.h>
 
-static char *sched_reg_p1_times = "8:45$1st$9:33";
-static char *sched_reg_p2_times = "9:26$2nd$10:02";
-static char *sched_reg_p3_times = "10:06$3rd$10:42";
-static char *sched_reg_p4_times = "10:46$4th$11:22";
-static char *sched_reg_p5_times = "11:26$5th$12:52";
-static char *sched_reg_p6_times = "12:56$6th$13:33";
-static char *sched_reg_p7_times = "13:37$1st$9:22";
+static char *sched_reg_p1_times = "8:45$1st$9:34";
+static char *sched_reg_p2_times = "9:38$2nd$10:27";
+static char *sched_reg_p3_times = "10:31$3rd$11:20";
+static char *sched_reg_p4_times = "11:24$4th$12:13";
+static char *sched_reg_p5_times = "11:24$5th$13:43";
+static char *sched_reg_p6_times = "13:47$6th$14:36";
+static char *sched_reg_p7_times = "14:40$7th$15:30";
